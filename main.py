e99b3f59b24c60b2d482eba961bf2d22d5292330
Michael Cruz <77006410+mrcruz117@users.noreply.github.com>
2021-10-15 20:08:21 +0000

    day1 project complete

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('goodbye earth')
+print('Band Name Generator')
+
+city = input('What city did you grow up in?\n')
+
+pet = input("What is your pet's name?\n")
+
+print('Band Name: ' +city+' '+pet)
+
+# day one project complete!
